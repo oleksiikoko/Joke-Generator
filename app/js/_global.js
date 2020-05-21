ca169a4869e4417cc3adc35fd05267edf1d555a0
Oleksii Kovalchuk <oleksiij.ko@gmail.com>
2020-05-21 17:34:39 +0300

    markup

---
+++ app/js/_global.js
@@ -0,0 +1,15 @@
+// app.global = {
+//   init: function () {
+//     // Load all global functions here
+//     console.log("load global functions");
+//     app.global.loadHeader();
+//   },
+//   loadHeader: function () {
+//     // Some specific function
+//     console.log("loadHeader()");
+//   },
+// };
+
+// // Run the global stuff
+// app.global.init();
+console.log("_global");
